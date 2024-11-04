8869b12d9bf415f3baddf4fe73b754d704e4f361
cactusballs <lottielucyjudge@gmail.com>
2024-11-04 19:59:24 +0000

    Navbar Skeleton

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home/Home"
+import NavBar from "./components/navbar/Navbar";
 
 
 function App() {
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
+import NavBar from '../navbar/Navbar';
 
 const Home = () => {
 
   return (
     <div>
+    <NavBar></NavBar>
     <h1>Buy It Buy Me! </h1>
     <p>Shop in real life nearby</p>
     <p>Support your local economy</p>
