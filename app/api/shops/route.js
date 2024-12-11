865493323f7e4cac30f8f0a280b9576284568322
cactusballs <lottielucyjudge@gmail.com>
2024-12-11 06:38:25 +0000

    sqli'n

--- app/api/shops/route.js
+++ app/api/shops/route.js
@@ -1,12 +1,19 @@
 import { shopConnection } from "../../../lib/db";
 import { NextResponse } from "next/server";
 
-export default async function GET() {
+export async function GET() {
   try {
     const db = await shopConnection()
     const sql = "SELECT * FROM shops"
     const [shops] = await db.query(sql)
-    return NextResponse.json(posts)
+
+    console.log("shops fetched:", shops);
+
+    if (shops.length === 0 ){
+      return NextResponse.json({ error: "No Shops Fam"})
+    }
+
+    return NextResponse.json(shops)
   } catch(error) {
     console.log(error)
     return NextResponse.json({error: error.message})
