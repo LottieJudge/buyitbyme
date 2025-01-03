cc03db5b5cfa39027bc9d2d31a1e9ef71d6c47d9
cactusballs <lottielucyjudge@gmail.com>
2025-01-03 16:40:47 +0000

    footer cont

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,3 +3,8 @@
 // importing the router and then adding it as an object re CodeCademy 
 import { useRouter } from "next/navigation";
 
+export default function Footer(){
+  const router = useRouter();
+
+  return 
+}
