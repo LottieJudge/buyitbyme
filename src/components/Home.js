8afb84272f7f20acc95afc7b59df3747574714a5
otskey <scgodfrey@hotmail.com>
2025-03-22 22:28:42 +0000

    Add AvatarIcon and LogoIcon components

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ import Footer from '@/components/Footer';
 
 export default function home() {
   return (
-    <main className="min-h-screen bg-gray-50 p-10">
+    <main className="min-h-screen bg-gray-50">
       <Navigation />
       <Hero />
       <Feature />
