2d966e71d04b22a6fa1b27ed83fd7800f9b557c4
cactusballs <lottielucyjudge@gmail.com>
2025-03-22 15:08:20 +0000

    update - removing code

--- app/lib/db.js
+++ app/lib/db.js
@@ -1,4 +1,3 @@
-// this is lib/db.js
 import mysql from "mysql2/promise"
 
 let connection;
@@ -13,26 +12,3 @@ export const dbConnection = async () => {
       }
       return connection
     };
- 
-// this is app/api/shops/route.js
-import { dbConnection } from "../../../lib/db";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  try {
-    const db = await dbConnection()
-    const sql = "SELECT * FROM shops"
-    const [shops] = await db.query(sql)
-
-    console.log("shops fetched:", shops);
-
-    if (shops.length === 0 ){
-      return NextResponse.json({ error: "No Shops Found"})
-    }
-
-    return NextResponse.json(shops)
-  } catch(error) {
-    console.log(error)
-    return NextResponse.json({error: error.message})
-  }
-};
