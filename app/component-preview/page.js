bf592a6583fd18cf08d4ce7557992ab879922af7
otskey <scgodfrey@hotmail.com>
2025-03-22 11:47:31 +0000

    Add Searchbar component and product listing components, footer component, home component, Navbar component and update to Component Preview page for testing

--- app/component-preview/page.js
+++ app/component-preview/page.js
@@ -1,12 +1,13 @@
 'use client';
 
 import Hero from '@/components/Hero';
-
+import Searchbar from '@/components/Searchbar'; 
 export default function ComponentPreviewPage() {
   return (
     <main className="min-h-screen bg-gray-50 p-10">
       <h1 className="text-2xl font-bold mb-6">Component Preview</h1>
       <Hero />
+      <Searchbar />
     </main>
   );
 }
